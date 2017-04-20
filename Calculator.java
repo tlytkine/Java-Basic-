ae53c74ab7a1577010dea285d4cb9d09949e3f99
tlytkine <tlytkine@gmail.com>
2017-04-20 19:01:29 -0400

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+
+public static int Multiplication(int x, int y){
+int product = x * y;
+return product;
+}
+public static int Division(int x, int y){
+int quotient = x / y;
+return quotient;
+}
+public static int Addition(int x, int y){
+int sum = x + y;
+return sum;
+}
+public static int Subtraction(int x, int y){
+int difference = x - y;
+return difference;
+}
+
+public static void main(String[]args){
+
+
+}
+}
