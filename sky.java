b6e55878a2c093ec58d4b63760beb8014f81525d
Timothy Lytkine <tlytkine@gmail.com>
2017-05-11 23:00:02 -0400

    Update sky.java

--- sky.java
+++ sky.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
 public class Sky {
 
+  Scanner scnr = new Scanner(System.in);
+  
+  
+  public static void main(String[]args) {
+    
+    Systen.out.println("Is the sky blue? - Enter yes or no.");
+    String x = scnr.next();
+    
+    if(x=="yes"){
+      System.out.println("Correct! Good job!.");
+    }
+    else {
+      System.out.println("Wrong! The sky is blue!!!!!!!");
+    }
+
+  }
+  
 
 }
